eeae5b19aa2da773b000d4a59716ae29eee2f2e1
Félix M <mil.felixx@gmail.com>
2019-07-14 01:23:18 +0200

    - added a 'by' argument to perfPrecision to perform the performance by slice.

--- R/performances.R
+++ R/performances.R
@@ -6,9 +6,10 @@
 #' references: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2556577/
 #' 
 #' @param df dataframe 
-#' @param x column name of the value to evaluate
-#' @param day column name where days ids are stored
-#' @param run column name where run ids are stored
+#' @param x string. column name of the value to evaluate
+#' @param day string. column name where days ids are stored
+#' @param run string. column name where run ids are stored
+#' @param by string, default to NULL. column name of a slicing variable. Use by if the table contains a variable to repeat the analysis on (for example a QC level or a gold-standard identification variable). If by is used, a list containing one result per slice level is returned.
 #'
 #' @return dataframe containing main statistics on variance
 #' @export 
@@ -21,42 +22,55 @@
 #' data(Glucose,package="VCA")
 #' perfPrecision(Glucose, 'result','run', 'day')
 #' 
-perfPrecision <- function(df, x, run, day) {
+perfPrecision <- function(df, x, run, day, by=NULL) {
+  
   res <- 
     anovaVCA(reformulate(paste(day,'/',run), response = x), 
-             df)
+             Data = df,
+             by = by)
   
-    res <- as.data.frame(res[["aov.tab"]])[c(1,5,6,7)]
+  if (length(res) > 1 & names(res[1]) != 'call'){
+    precisionResults <- list()
+    for (i in seq_along(res)) {
+      
+      precisionResults[[names(res[i])]] <- as.data.frame(res[[i]][["aov.tab"]])[c(1,5,6,7)]
+      
+      rownames(precisionResults[[names(res[i])]]) = c('Withing laboratory', 'within-day', 'within-run', 'error')
+      
+    }
+  } else {
+    precisionResults <- as.data.frame(res[["aov.tab"]])[c(1,5,6,7)]
     
-    rownames(res) = c('Withing laboratory', 'within-day', 'within-run', 'error')
-    
-    res
+    rownames(precisionResults) = c('Withing laboratory', 'within-day', 'within-run', 'error')
+  }
   
-## Tried to reimplement  manually but found the VCA package
-# n_day <-  n_distinct(df %>% select(!!day))
-# 
-# n_rep <- df %>%
-#   group_by(!!day) %>%
-#   summarise(c=n()) %>%
-#   ungroup() %>%
-#   pull(mean(c)) %>% unique()
-# 
-# s_r <- df %>%
-#   group_by(!!day) %>%
-#   mutate(errorMean = (!!x - mean(!!x))^2) %>%
-#   ungroup() %>%
-#   mutate(s_r= sqrt(sum(errorMean)/n_day*(n_rep-1))) %>%
-#   distinct(s_r) %>%
-#   pull()
-# 
-# s_b <- df %>%
-#   group_by(!!day) %>%
-#   mutate(mean= mean(!!x)) %>%
-#   ungroup() %>%
-#   mutate(s_b = sum((mean-mean(!!x))^2)/(n_day-1)) %>%
-#   distinct(s_b) %>%
-#   pull()
-# 
-# s_l <- df %>%
-#   summarize(sqrt(((n_rep-1/n_rep)*(s_b+s_r)))) %>% pull()
+  return(precisionResults)
+  
+  ## Tried to reimplement  manually but found the VCA package
+  # n_day <-  n_distinct(df %>% select(!!day))
+  # 
+  # n_rep <- df %>%
+  #   group_by(!!day) %>%
+  #   summarise(c=n()) %>%
+  #   ungroup() %>%
+  #   pull(mean(c)) %>% unique()
+  # 
+  # s_r <- df %>%
+  #   group_by(!!day) %>%
+  #   mutate(errorMean = (!!x - mean(!!x))^2) %>%
+  #   ungroup() %>%
+  #   mutate(s_r= sqrt(sum(errorMean)/n_day*(n_rep-1))) %>%
+  #   distinct(s_r) %>%
+  #   pull()
+  # 
+  # s_b <- df %>%
+  #   group_by(!!day) %>%
+  #   mutate(mean= mean(!!x)) %>%
+  #   ungroup() %>%
+  #   mutate(s_b = sum((mean-mean(!!x))^2)/(n_day-1)) %>%
+  #   distinct(s_b) %>%
+  #   pull()
+  # 
+  # s_l <- df %>%
+  #   summarize(sqrt(((n_rep-1/n_rep)*(s_b+s_r)))) %>% pull()
 }
--- tests/testthat/test-performances.R
+++ tests/testthat/test-performances.R
@@ -4,5 +4,10 @@ library(VCA)
 
 test_that("Precision works", {
   data(Glucose,package="VCA")
+  Glucose2 <- rbind(Glucose, Glucose)
+  Glucose2['lot'] <- rep(c('A','B'), each=80)
   expect_equal(round(perfPrecision(Glucose, 'result','run', 'day')[1,1],2),round(64.77732,2))
+  expect_condition(names(perfPrecision(Glucose, 'result','run', 'day', by='lot')[2]) == "lot.B")
+  
+  
 })
